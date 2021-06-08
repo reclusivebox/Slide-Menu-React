a37a929e76e6f38ef086d1b578d0a0a065701e7a
Bruno Gregório <Bruno08new12@gmail.com>
2021-06-08 11:00:57 -0300

    Added backdrop dynamic shadow

--- src/SlideMenu.tsx
+++ src/SlideMenu.tsx
@@ -1,29 +1,61 @@
 // eslint-disable-next-line no-use-before-define
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styles from './styles/SlideMenu.module.scss';
 
 type SlideMenuProps = React.PropsWithChildren<{}>;
 
-function ajustPosition(event: React.SyntheticEvent) {
-  (event.target as Element).scrollIntoView({
-    inline: 'start',
-  });
+/**
+ * React Hook to ajust the menu position on render
+ */
+function usePositionAjuster(ref: React.MutableRefObject<null>) {
+  useEffect(() => {
+    (ref.current as unknown as Element).scrollIntoView({
+      inline: 'center',
+    });
+  }, []);
+}
+
+function useOpacityAjuster(
+  menuRef: React.MutableRefObject<null>,
+  backdropRef: React.MutableRefObject<null>,
+) {
+  function callback(entries: IntersectionObserverEntry[]) {
+    const backdrop = (backdropRef.current as unknown as HTMLElement);
+    const showRatio = entries[0].intersectionRatio;
+    backdrop.style.opacity = `${showRatio / 2}`;
+
+    if (showRatio > 0.1 && showRatio < 0.2 && entries[0].isIntersecting) {
+      backdrop.style.display = 'block';
+    } else if (showRatio < 0.2 && !(entries[0].isIntersecting)) {
+      backdrop.style.opacity = '0';
+      setTimeout(() => {
+        backdrop.style.display = 'none';
+      }, 500);
+    }
+  }
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callback, {
+      threshold: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+    });
+
+    observer.observe((menuRef.current as unknown as HTMLElement));
+  }, []);
 }
 
 export default function SlideMenu({ children }: SlideMenuProps) {
-  // return (
-  //   <div className={styles.menuOverlay}>
-  //     <div className={styles.menuContainer}>
-  //       <div className={styles.menuContent}>{children}</div>
-  //       <div className={styles.placeholder} onLoad={ajustPosition} />
-  //     </div>
-  //   </div>
-  // );
+  const placeholderRef = useRef(null);
+  const menuRef = useRef(null);
+  const backdropRef = useRef(null);
+
+  usePositionAjuster(placeholderRef);
+  useOpacityAjuster(menuRef, backdropRef);
+
   return (
     <div className={styles.menuGrid}>
-      <div className={styles.menuContainer}>{children}</div>
-      <div className={styles.placeholder} />
-      <div className={styles.backdrop} />
+      <div className={styles.menuContainer} ref={menuRef}>{children}</div>
+      <div className={styles.placeholder} ref={placeholderRef} />
+      <div className={styles.backdrop} ref={backdropRef} />
     </div>
   );
 }
