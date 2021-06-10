eb2c3721431d4c76bc4c8e98a27aec61a7666c9e
Bruno Gregório <Bruno08new12@gmail.com>
2021-06-10 15:00:33 -0300

    Added JSDocs to the hooks file

--- src/hooks.ts
+++ src/hooks.ts
@@ -8,7 +8,8 @@ function useMobileEffect(callback: Function) {
 }
 
 /**
- * React Hook to ajust the menu position on render
+ * A react Hook to scroll the menu on Render.
+ * @param ref - The Ref for the element you want to centralize on render.
  */
 export function usePositionAjuster(ref: React.MutableRefObject<null>) {
   useMobileEffect(() => {
@@ -18,6 +19,12 @@ export function usePositionAjuster(ref: React.MutableRefObject<null>) {
   });
 }
 
+/**
+ * A react Hook to schedule callbacks to be ran when the menu is lauched or when it is hidden.
+ * @param menuRef - The Ref for the actual menu (the one with the children).
+ * @param activationCallbacks - A list of functions to be called when the menu is activated.
+ * @param deactivationCallbacks - A list of functions to be called when the menu is deactivated.
+ */
 export function useToggleEffect(
   menuRef: React.MutableRefObject<null>,
   activationCallbacks: Function[] = [],
